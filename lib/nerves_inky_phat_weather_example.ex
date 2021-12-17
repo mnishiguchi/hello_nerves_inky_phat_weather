e5a4b899a4b5d60a5392dbebc3d6a7f3221f3b69
Masatoshi N <nishiguchi.masa@gmail.com>
2021-12-17 18:20:37 -0500

    Init

---
+++ lib/nerves_inky_phat_weather_example.ex
@@ -0,0 +1,18 @@
+defmodule NervesInkyPhatWeatherExample do
+  @moduledoc """
+  Documentation for NervesInkyPhatWeatherExample.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NervesInkyPhatWeatherExample.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nerves_inky_phat_weather_example_test.exs
@@ -0,0 +1,8 @@
+defmodule NervesInkyPhatWeatherExampleTest do
+  use ExUnit.Case
+  doctest NervesInkyPhatWeatherExample
+
+  test "greets the world" do
+    assert NervesInkyPhatWeatherExample.hello() == :world
+  end
+end
